a2ac0e8abf1928c9a2ffeb496400931aa57d8c25
Phil Hardy <philiphardy4444@gmail.com>
2025-11-17 16:10:39 -0800

    Calibration and individual motor adjustment. Fixed pitch calculation

--- flight_control.c
+++ flight_control.c
@@ -15,19 +15,33 @@
 
 #define LOOP_SLEEP_MS 5
 
-static void flight_control_begin();
+#define CALIBRATION_CYCLES 250
+
+typedef enum {
+    CALIBRATING,
+    OPERATING
+} state_t;
+
+static void task_entry();
+static inline float radians_to_degrees(float r);
+static void calibrate_starting_offsets();
 static void complementary_filter_update(vector_3d * accel, vector_3d * gyro, float dt);
 static void notify_motor_adjustment();
+static inline float get_pitch();
+static inline float get_roll();
+
+static state_t state = CALIBRATING;
 
 // Current fused angles (degrees)
 static float pitch = 0.0f;
 static float roll  = 0.0f;
+static float pitch_offset = 0.0f;
+static float roll_offset  = 0.0f;
 
 void flight_control_init() {
     mpu6050_init();
     motor_adjustment_init();
-    // TODO: Calibrate to zero out starting roll and pitch
-    flight_control_begin();
+    task_entry();
 }
 
 static inline float radians_to_degrees(float r) {
@@ -42,50 +56,67 @@ static void complementary_filter_update(vector_3d * accel, vector_3d * gyro, flo
     float ay = accel->y / ACCEL_SENS_2G;
     float az = accel->z / ACCEL_SENS_2G;
 
+    // --- Accelerometer angles ---
+    // Pitch = rotation around Y axis
+    float pitch_acc = radians_to_degrees(atan2f(ax, az));
+
+    // Roll = rotation around X axis
+    float roll_acc  = radians_to_degrees(atan2f(ay, az));
+
     // Convert gyro to deg/sec
     float gx = gyro->x / GYRO_SENS_250DPS;
     float gy = gyro->y / GYRO_SENS_250DPS;
 
-    // --- Accelerometer angles ---
-    // Pitch = rotation around Y axis
-    float pitch_acc = radians_to_degrees(atan2f(ax, sqrtf(ay * ay + az * az)));
-    // Roll = rotation around X axis
-    float roll_acc  = radians_to_degrees(atan2f(ay, az));
-
     // --- Gyro integration (predict) ---
-    pitch -= gx * dt;     // gx rotates pitch
-    roll  += gy * dt;     // gy rotates roll
+    pitch += gy * dt;     // gy rotates pitch
+    roll  += gx * dt;     // gx rotates roll
 
     // --- Complementary filter blend ---
     pitch = ALPHA * pitch + (1.0f - ALPHA) * pitch_acc;
     roll  = ALPHA * roll  + (1.0f - ALPHA) * roll_acc;
 }
 
-static void flight_control_begin() {
-    uint32_t lastTime = 0;
+static inline float get_pitch() {
+    return pitch - pitch_offset;
+}
+
+static inline float get_roll() {
+    return roll - roll_offset;
+}
+
+static void task_entry() {
+    uint32_t last_time = 0;
+    uint8_t cycles = 0;
 
     while (true) {
         vector_3d acceleration = mpu6050_read_acceleration();
         vector_3d gyro = mpu6050_read_gyro();
 
         uint32_t now = time_us_32();
-        float dt = (now - lastTime) / MICROSEC_PER_SEC; // Convert to seconds
-        lastTime = now;
+        float dt = (now - last_time) / MICROSEC_PER_SEC; // Convert to seconds
+        last_time = now;
 
         complementary_filter_update(&acceleration, &gyro, dt);
 
-        notify_motor_adjustment();
+        if (CALIBRATING == state && cycles >= CALIBRATION_CYCLES) {
+            state = OPERATING;
+            pitch_offset = pitch;
+            roll_offset = roll;
+        } else if (OPERATING == state) {
+            // Pass pitch and roll to the motor adjustment task
+            notify_motor_adjustment();
+        }
 
-        // Pass pitch and roll to the motor adjustment task
         sleep_ms(LOOP_SLEEP_MS);
+        cycles++;
     }
 }
 
 static void notify_motor_adjustment() {
-    uint32_t p = *(uint32_t *)&pitch;
-    uint32_t r = *(uint32_t *)&roll;
     // DO NOT REORDER!!!
     // The motor adjustment task relies on the queue being structured this way
-    multicore_fifo_push_blocking(p);
-    multicore_fifo_push_blocking(r);
+    float pitch = get_pitch();
+    multicore_fifo_push_blocking(*(uint32_t *)&pitch);
+    float roll = get_roll();
+    multicore_fifo_push_blocking(*(uint32_t *)&roll);
 }
--- motor_adjustment.c
+++ motor_adjustment.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/mem_ops.h"
 #include "pico/multicore.h"
 
+#define MAX_POWER 100
+#define MAX_ANGLE 90
+
 static void task_entry();
+static void adjust_front_motor(float pitch);
+static void adjust_back_motor(float pitch);
+static void adjust_left_motor(float roll);
+static void adjust_right_motor(float roll);
+static float normalize_power(float p);
+
+static float hovering_power;
 
 void motor_adjustment_init() {
     // start task on core 1
@@ -16,5 +27,75 @@ static void task_entry() {
         msg = multicore_fifo_pop_blocking();
         float roll = *(float *)&msg;
         printf("Pitch: %.2f  Roll: %.2f\n", pitch, roll);
+
+        adjust_front_motor(pitch);
+        adjust_back_motor(pitch);
+        adjust_left_motor(roll);
+        adjust_right_motor(roll);
     }
+}
+
+static float front_motor_power;
+static void adjust_front_motor(float pitch) {
+    float multiplier = fabsf(pitch / MAX_ANGLE);
+    if (pitch < 0) {
+        // nose is pitching down -- increase motor power
+        multiplier += 1;
+    } else {
+        // nose is pitching up -- decrease motor power
+        multiplier = 1 - multiplier;
+    }
+    front_motor_power = hovering_power * multiplier;
+    front_motor_power = normalize_power(front_motor_power);
+    printf("FP: %.2f\n", multiplier);
+}
+
+static float back_motor_power;
+static void adjust_back_motor(float pitch) {
+    float multiplier = fabsf(pitch / MAX_ANGLE);
+    if (pitch > 0) {
+        // rear is pitching down -- increase motor power
+        multiplier += 1;
+    } else {
+        // rear is pitching up -- decrease motor power
+        multiplier = 1 - multiplier;
+    }
+    back_motor_power = hovering_power * back_motor_power;
+    back_motor_power = normalize_power(back_motor_power);
+    printf("BP: %.2f\n", multiplier);
+}
+
+static float left_motor_power;
+static void adjust_left_motor(float roll) {
+    float multiplier = fabsf(roll / MAX_ANGLE);
+    if (roll < 0) {
+        // rear is pitching down -- increase motor power
+        multiplier += 1;
+    } else {
+        // rear is pitching up -- decrease motor power
+        multiplier = 1 - multiplier;
+    }
+    left_motor_power = hovering_power * multiplier;
+    left_motor_power = normalize_power(left_motor_power);
+    printf("LP: %.2f\n", multiplier);
+}
+
+static float right_motor_power;
+static void adjust_right_motor(float roll) {
+    float multiplier = fabsf(roll / MAX_ANGLE);
+    if (roll > 0) {
+        multiplier += 1;
+    } else {
+        // rear is pitching up -- decrease motor power
+        float multiplier = 1 - multiplier;
+    }
+    right_motor_power = hovering_power * multiplier;
+    right_motor_power = normalize_power(right_motor_power);
+    printf("RP: %.2f\n", multiplier);
+}
+
+static float normalize_power(float p) {
+    p = MAX(0, p);
+    p = MIN(MAX_POWER, p);
+    return p;
 }
